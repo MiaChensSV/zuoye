1e7af76ce22affbbb637d2c6d48affbb92abf9ba
Hong-Sun2018 <38651634+Hong-Sun2018@users.noreply.github.com>
2022-12-14 14:33:24 +0100

    finished basic set up and authentiucation

---
+++ frontend/infrastructure/test/settings.tfvars
@@ -0,0 +1,11 @@
+# Project settings
+project="gcp-study-hong-sun"
+product="zuoye"
+component_name="frontend"
+environment="test"
+location="EU"
+region="europe-north1"
+zone="europe-north1-c"
+
+#Terraform settings
+terraform_sa="terraform-sa@primal-stock-338302.iam.gserviceaccount.com"
---
+++ frontend/infrastructure/variables.tf
@@ -0,0 +1,24 @@
+variable "project" {
+  type = string
+}
+variable "product" {
+  type = string
+}
+variable "component_name" {
+  type = string
+}
+variable "environment" {
+  type = string
+}
+variable "tf_state_bucket" {
+  type = string
+}
+variable "location" {
+  type = string
+}
+variable "region" {
+  type = string
+}
+variable "zone" {
+  type = string
+}
